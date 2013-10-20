e204e900b1b93c63951988e3cfcb70f44147807e
Ruben Panadero Navarrete <rubpana@gmail.com>
2013-10-21 00:03:32 +0200

    Integrado el seguimiento de videos y el paso de usuario por las diferentes ventanas.

--- FitBox/src/fitbox/controller/dao/Dal.java
+++ FitBox/src/fitbox/controller/dao/Dal.java
@@ -1,49 +1,4 @@
-<<<<<<< HEAD
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package fitbox.controller.dao;
 
-import fitbox.model.Actividad;
-import fitbox.model.Usuario;
-import fitbox.model.Video;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-/**
- *
- * @author Elias
- */
-public class Dal {
-    private static Dal dal;
-    private static Map<String,Dao> daos;
-    
-    private Dal(){
-        daos =  new HashMap<String,Dao>();
-        daos.put("Usuario", new Dao<Usuario>(Usuario.class));
-        daos.put("Actividad",new Dao<Actividad>(Actividad.class));
-        daos.put("Video",new Dao<Video>(Video.class));
-    }
-    
-    public static Dal getDal(){
-    if(dal==null) dal = new Dal();
-    return dal;
-    }
-    
-    public List find(String consulta,Object[] parametros,Class tipoClase){
-        Dao dao = daos.get(tipoClase.getSimpleName());
-        return dao.find(consulta,parametros);
-    }
-
-    public void insert(Usuario usuario) {
-       Dao dao = daos.get(usuario.getClass().getSimpleName());
-       dao.insert(usuario); 
-    }
-
-}
-=======
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -88,4 +43,4 @@ public class Dal {
     }
 
 }
->>>>>>> f127ffa5a167d6d306f986ba9b541b0d2b29d157
+
