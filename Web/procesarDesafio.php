405f0c7698a43587b8878d871a1e2967f6938e5f
Jose <jopetor2@gmail.com>
2013-12-11 09:22:12 +0100

    Web

--- Web/procesarDesafio.php
+++ Web/procesarDesafio.php
@@ -3,20 +3,15 @@ include_once("conexion.php");
 
 $name=$_POST['nombreEvento'];
 $actividad=$_POST['selCombo'];
+echo $actividad;
 $dateInicio=$_POST['fecha_inicio'];
 $dateFin=$_POST['fecha_fin'];
-//echo $dateFin;
-list($año, $mes, $dia) = split('[/.-]', $dateFin);
-$fecha=$dia."/".$mes."/".$año;
-//echo $fecha;
 $amigoSeleccionado=$_POST['rival'];
-$log=$_POST['usuario'];
-//$amigo=$amigoSeleccionado.substr(0,strlen($amigoSeleccionado)-1);
-//echo "ok";
 
-$sql="insert into `desafio`(`idDesafio`,`nombre`,`fechaInicio`,`fechaFin`,`estado`,`idUsuario`,`idRival`,`idActividad`) values(null,'$name','$dateInicio','$fecha',0,$log,$amigoSeleccionado,$actividad)";
+$sql="insert into `desafio`(`idDesafio`,`nombre`,`fechaInicio`,`fechaFin`,`estado`,`idUsuario`,`idRival`,`idActividad`) values(null,'$name','$dateInicio','$dateFin',0,$amigoSeleccionado,3,$actividad)";
 mysql_query($sql);
+echo $sql;
 mysql_close();
-header("Location: desafios.html");
+
 
 ?>
